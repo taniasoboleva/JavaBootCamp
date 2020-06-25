1a27ef40b8986328c09163c9e1c64508a2626d2d
taniasoboleva <tanjusja11@mail.ru>
2020-06-25 10:34:58 +0300

    Exercise 1, day 3

--- SEF/src/sef/module6/activity/Employee_I.java
+++ SEF/src/sef/module6/activity/Employee_I.java
@@ -4,21 +4,36 @@ package sef.module6.activity;
 public class Employee_I  {
 	
 	//Attributes
-	private double salary;
+	private int salary;
 	private String title;
 	
 	//Behavior - write default constructor. Print 'I'm an Employee_I Constructor'
-	
+	public Employee_I(){ 
+		System.out.println("I'm Student_I constructor"); 
+	}
 	
 	// write getter for int salary
-	
+	public int getSalary() {
+		return salary ;
+	}
 	
 	// write setter for int salary
-	
-	
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 	// write getter for String title
-	
+		public String getTitle() {
+			return title;
+		}
 	
 	// write setter for String title
 	
+public void setTitle(String title) {
+	this.title = title;
+}
+
+public void announce(){
+	super.announce();  // calls announce() method of Person class
+	System.out.println("Inside Person Class");
+
 }
--- SEF/src/sef/module6/activity/InheritanceActivity.java
+++ SEF/src/sef/module6/activity/InheritanceActivity.java
@@ -1,6 +1,6 @@
 package sef.module6.activity;
 
-
+import sef.module6.sample.Person_I;
 
 public class InheritanceActivity {
 
@@ -9,13 +9,19 @@ public class InheritanceActivity {
 		// Create Person object p. Write code to and set name of Person as Sarah Johnson
 		// and age as 21
 		
-		
+		Person_I p = new Person_I () ;
+		p.setName("Sarah Johnson");
+		p.setAge(21);
 		
 		System.out.println("-----------------------------");
 		//Create Employee object e and 
 		// set salary as 70000, title as Developer, age as 32 and name as Shawn Cun
 		
-		
+		Empoyee_I e = new Empoyee_I () ;
+		e.setSalary(70000);
+		e.setTitle("Developer");
+		e.setAge(32);
+		e.setName("Shawn Cun");
 		
 		// Print Info using Employee object
 		System.out.println("-----------------------------");
@@ -29,7 +35,7 @@ public class InheritanceActivity {
 		System.out.println("-----------------------------");
 		System.out.println("Person's Name is    : " + p.getName());
 		System.out.println("Person's Age is     : " + p.getAge());
-		
+		s.announce();
 			
 		
 	}
